6052715b7b309a196e03e30b398c2a16735c425c
Ivanka <idvoryanska@gmail.com>
2021-10-30 18:02:01 +0200

    register form without styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import ContactForm from "./ContactForm/ContactForm";
-import ContactList from "./ContactList/ContactList";
-import Filter from "./Filter/Filter";
-import { PrimaryTitle, SecondaryTitle } from "./App.styled";
+// import ContactForm from "./ContactForm/ContactForm";
+// import ContactList from "./ContactList/ContactList";
+// import Filter from "./Filter/Filter";
+import RegisterForm from "./RegisterForm/RegisterForm";
+// import { PrimaryTitle, SecondaryTitle } from "./App.styled";
 
 function App() {
   return (
     <>
-      <PrimaryTitle>Phonebook</PrimaryTitle>
+      <RegisterForm />
+      {/* <PrimaryTitle>Phonebook</PrimaryTitle>
       <ContactForm />
       <SecondaryTitle>Contacts</SecondaryTitle>
       <Filter />
-      <ContactList />
+      <ContactList /> */}
     </>
   );
 }
---
+++ src/RegisterForm/RegisterForm.js
@@ -0,0 +1,78 @@
+import { Formik, Field, Form, ErrorMessage } from "formik";
+
+const validate = (values) => {
+  const errors = {};
+  if (!values.userName) {
+    errors.userName = "Required";
+  } else if (values.userName.length > 15) {
+    errors.userName = "Must be 15 characters or less";
+  }
+
+  if (!values.userPassword) {
+    errors.userPassword = "Required";
+  } else if (values.userPassword.length < 6) {
+    errors.userPassword = "Must be 6 characters or more";
+  } else if (values.userPassword.length > 12) {
+    errors.userPassword = "Must be 20 characters or less";
+  }
+
+  if (!values.userPasswordConfirm) {
+    errors.userPasswordConfirm = "Required";
+  } else if (values.userPasswordConfirm !== values.userPassword) {
+    errors.userPasswordConfirm = "Password and confirm password don't match";
+  }
+
+  if (!values.userEmail) {
+    errors.userEmail = "Required";
+  } else if (
+    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.userEmail)
+  ) {
+    errors.userEmail = "Invalid email address";
+  }
+
+  return errors;
+};
+
+const RegisterForm = () => {
+  return (
+    <Formik
+      initialValues={{
+        userName: "",
+        userEmail: "",
+        userPassword: "",
+        userPasswordConfirm: "",
+      }}
+      validate={validate}
+      onSubmit={(values, formikBag) => {
+        console.log(values);
+        formikBag.resetForm();
+      }}
+    >
+      {({ isValid, dirty }) => (
+        <Form>
+          <label htmlFor="userName">Name</label>
+          <Field name="userName" type="text" />
+          <ErrorMessage name="userName" />
+
+          <label htmlFor="userEmail">Email</label>
+          <Field name="userEmail" type="email" />
+          <ErrorMessage name="userEmail" />
+
+          <label htmlFor="userPassword">Password</label>
+          <Field name="userPassword" type="password" />
+          <ErrorMessage name="userPassword" />
+
+          <label htmlFor="userPasswordConfirm">Confirm password</label>
+          <Field name="userPasswordConfirm" type="password" />
+          <ErrorMessage name="userPasswordConfirm" />
+
+          <button type="submit" disabled={!isValid || !dirty}>
+            Register
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default RegisterForm;
