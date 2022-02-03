9f82437897d74ea27556f2c3a1e4238c8fe0a6f3
Ivanka <idvoryanska@gmail.com>
2022-02-03 20:45:55 +0100

    added images to home page

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,6 +1,27 @@
-import { Title } from "./HomePage.styled";
+import {
+  Title,
+  Wrapper,
+  FirstImg,
+  SecondImg,
+  ThirdImg,
+} from "./HomePage.styled";
+import firstImg from "../images/photomin1.png";
+import secondImg from "../images/photomin2.png";
+import thirdImg from "../images/photomin3.png";
 const HomePage = () => {
-  return <Title> Welcome to Phonebook!</Title>;
+  return (
+    <Wrapper>
+      <FirstImg
+        src={`${firstImg}`}
+        alt="contacts list"
+        width="250px"
+        height="150px"
+      />
+      <SecondImg src={`${secondImg}`} alt="" width="250px" height="150px" />
+      <ThirdImg src={`${thirdImg}`} alt="" width="250px" height="150px" />
+      <Title> Welcome to Phonebook!</Title>
+    </Wrapper>
+  );
 };
 
 export default HomePage;
